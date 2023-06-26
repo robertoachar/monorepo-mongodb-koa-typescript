ba0be527ec017fe8123422deb53236f52145409e
Roberto Achar <robertoachar@gmail.com>
2023-06-26 14:31:03 -0300

    refactor: add packages to jest config

--- jest-eslint.config.ts
+++ jest-eslint.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialProjectOptions = {
   displayName: 'lint',
   runner: 'jest-runner-eslint',
-  testMatch: ['<rootDir>/*.ts', '<rootDir>/src/**/*.ts'],
+  testMatch: ['<rootDir>/*.ts', '<rootDir>/packages/**/*.ts'],
 };
 
 export default config;
